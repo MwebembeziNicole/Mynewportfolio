2a2abb7ab21adeb1cf7f7227ae99aaf418ebb78c
MwebembeziNicole <nicolemwebembezi18@gmail.com>
2025-03-31 10:24:47 +0300

    init

--- src/components/profile.js
+++ src/components/profile.js
@@ -10,7 +10,7 @@ const Profile = () => {
           <img src={profileImage} alt="MNM" width="300" height="300" />
           <h2>MWEBEMBEZI NICOLE MBABAZI</h2>
           <h4>
-            I am 22 years old, a female student at Uganda Christian University
+            I am 23 years old, a female student at Uganda Christian University
             offering a Bachelor of Science in Computer Science. My passion in
             the IT world is Data Care and Cybersecurity. I enjoy watching movies
             and hanging out with my friends.
